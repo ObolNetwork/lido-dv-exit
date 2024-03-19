3ed89ddc708f363c0836bedce5124975a54f11f9
Gianguido Sora <gsora@noreply.github.com>
2024-03-19 16:02:49 +0100

    make pre-commit happy

--- app/keystore/keystore_test.go
+++ app/keystore/keystore_test.go
@@ -1,3 +1,5 @@
+// Copyright © 2022-2024 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
+
 // Copyright © 2022-2023 Obol Labs Inc. Licensed under the terms of a Business Source License 1.1
 
 package keystore_test
